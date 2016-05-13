2dd1d2cbe9656c735544e1947d3e80a7a45d99a7
Samuel Yee <Samuel.Yee@thomsonreuters.com>
2016-05-13 14:47:19 -0400

    Contact Overlays Maps Now

--- js/style.js
+++ js/style.js
@@ -1,6 +1,3 @@
-
-
-
 $(function (){
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
